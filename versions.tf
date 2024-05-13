ea7767bc0364852aa0d5e549317eb487be52ab98
roeldev <roeldev@users.noreply.github.com>
2024-05-13 22:16:05 +0200

    Update versions

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ variable "talos_version" {
   # https://github.com/siderolabs/talos/releases
   description = "Talos version to use"
   type        = string
-  default     = "1.6.6"
+  default     = "1.7.1"
 }
 
 variable "talos_machine_install_image_url" {
@@ -23,26 +23,26 @@ variable "talos_ccm_version" {
   # https://github.com/siderolabs/talos-cloud-controller-manager/releases
   description = "Talos Cloud Controller Manager version to use"
   type        = string
-  default     = "1.4.0"
+  default     = "1.6.0"
 }
 
 variable "cilium_version" {
   # https://helm.cilium.io/
   description = "Cilium Helm version to use"
   type        = string
-  default     = "1.15.2"
+  default     = "1.15.4"
 }
 
 variable "argocd_version" {
   # https://github.com/argoproj/argo-cd/releases
   description = "ArgoCD version to use"
   type        = string
-  default     = "2.10.4"
+  default     = "2.11.0"
 }
 
 variable "metrics_server_version" {
   # https://github.com/kubernetes-sigs/metrics-server/releases
   description = "Kubernetes Metrics Server version to use"
   type        = string
-  default     = "0.7.0"
+  default     = "0.7.1"
 }
